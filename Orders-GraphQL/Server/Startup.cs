f46c6dd8397977e850ddbd60781a04885f6fe741
Nick Moyer <moyer.n.p@gmail.com>
2020-04-01 11:14:08 -0500

    Added the Order and Customer services

--- Orders-GraphQL/Server/Startup.cs
+++ Orders-GraphQL/Server/Startup.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Orders.Services;
 
 namespace Server
 {
@@ -15,6 +11,8 @@ namespace Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IOrderService, OrderService>();
+            services.AddSingleton<ICustomerService, CustomerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
