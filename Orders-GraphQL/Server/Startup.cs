a8946b64ae4e696243607b2097786a6308fa425d
Nick Moyer <moyer.n.p@gmail.com>
2020-04-01 10:51:58 -0500

    Added packages

--- Orders-GraphQL/Server/Startup.cs
+++ Orders-GraphQL/Server/Startup.cs
@@ -24,11 +24,8 @@ namespace Server
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
